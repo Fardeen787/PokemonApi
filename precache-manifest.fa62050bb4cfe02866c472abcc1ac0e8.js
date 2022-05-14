7ce7e073112aad105caa4fa07209537252efea92
Fardeen Khan <ffkhan@mitaoe.ac.in>
2022-05-14 23:26:30 +0530

    Updates

---
+++ precache-manifest.fa62050bb4cfe02866c472abcc1ac0e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad787b4626d1fd93f8e7c0d18f89fa2",
+    "url": "/PokemonApi/index.html"
+  },
+  {
+    "revision": "e1100be4921d729916fe",
+    "url": "/PokemonApi/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a6b63f7c592c67569104",
+    "url": "/PokemonApi/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "e1100be4921d729916fe",
+    "url": "/PokemonApi/static/js/2.f14c6944.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/PokemonApi/static/js/2.f14c6944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b63f7c592c67569104",
+    "url": "/PokemonApi/static/js/main.0ac88c7a.chunk.js"
+  },
+  {
+    "revision": "6a9f5d814bd79df7fcba",
+    "url": "/PokemonApi/static/js/runtime-main.86e78d9e.js"
+  }
+]);
